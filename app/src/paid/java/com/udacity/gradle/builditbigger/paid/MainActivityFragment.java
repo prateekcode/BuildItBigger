675dc951e4fa0553ce14baa71d824359e5ce4b62
Prateek <pratiekray@gmail.com>
2020-05-19 22:55:22 +0530

    unexpected app crash

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/paid/MainActivityFragment.java
@@ -0,0 +1,27 @@
+package com.udacity.gradle.builditbigger.paid;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.udacity.gradle.builditbigger.R;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment {
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
